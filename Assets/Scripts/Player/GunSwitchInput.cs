f96f84732d5b885301bb00a07cb268432aedd899
Netpune <113001568+Netpune@users.noreply.github.com>
2022-09-14 15:53:06 +1000

    Created gunswitch script

--- Assets/Scripts/Player/GunSwitchInput.cs
+++ Assets/Scripts/Player/GunSwitchInput.cs
@@ -7,6 +7,18 @@ public class GunSwitchInput : MonoBehaviour
     private void Update()
     {
         // If the player presses 1 (KeyCode.Alpha1) switch the GunSwitchManager.CurrentGun to GunSwitchManager.Gun.Pistol. If the player presses 2 (KeyCode.Alpha2) switch it to GunSwitchManager.Gun.M16 and if 3 (KeyCode.Alpha3) switch it to GunSwitchManager.Gun.Shotgun
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GunSwitchManager.CurrentGun = GunSwitchManager.Gun.Pistol;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GunSwitchManager.CurrentGun = GunSwitchManager.Gun.M16;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            GunSwitchManager.CurrentGun = GunSwitchManager.Gun.Shotgun;
+        }
     }
 }
