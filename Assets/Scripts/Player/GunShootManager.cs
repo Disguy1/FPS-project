f69aef12ff537224c7fb17dce6808e8f885696f5
ThefrlKanyeEast <98255167+ThefrlKanyeEast@users.noreply.github.com>
2022-09-14 16:31:23 +1000

    PEWPEW Made

--- Assets/Scripts/Player/GunShootManager.cs
+++ Assets/Scripts/Player/GunShootManager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class GunShootManager : MonoBehaviour
 {
     // Have a public variable for the player camera caled PlayerCam.
+    public Camera playercam;
+
 
 
     public void Shoot(float Damage, float Range)
     {
         // Shoot a raycast from the centre of the PlayerCam within the range of Range. If the raycast hits an object with a tag of Zombie, get the Zombie component from it and call TakeDamage() while passing the Damage variable.
-
+        RaycastHit hit;
+        if (Physics.Raycast(playercam.transform.position, playercam.transform.TransformDirection(Vector3.forward), out hit, Range))
+        {
+            if (hit.transform.tag == "Zombie")
+            {
+                hit.transform.GetComponent<Zombie>().TakeDamage(Damage); 
+            }
+        }
+        
     }
 }
