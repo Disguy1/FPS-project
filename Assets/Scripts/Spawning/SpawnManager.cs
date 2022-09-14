06fc1877bbab6d27535e4994d8d2e833432d9f01
Richie1 <33795818+richiever@users.noreply.github.com>
2022-09-14 16:47:59 +1000

    Zombie Spawner
    Zombie Spawner

--- Assets/Scripts/Spawning/SpawnManager.cs
+++ Assets/Scripts/Spawning/SpawnManager.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Have a public variable of a new GameObject caled Zombie
+    public GameObject zombie;
     
 
     public void Spawn()
     {
         // Instantiate the Zombie object with position of SpawnPosition.RandomPos().
-        
+        Instantiate (zombie, SpawnPosition.RandomPos(), Quaternion.identity);
     }
 }
